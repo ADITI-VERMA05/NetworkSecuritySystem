735fcfe13563ca7650ad913f2a851ec676157c69
ADITI_VERMA05 <aditiverma1605@gmail.com>
2025-11-24 15:50:18 +0530

    Data ingestion component

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def get_requirements()->List[str]:
     return requirement_lst
 
 setup(
-    name="NetworkSecuritySystem",
+    name="NetworkSecurity",
     version="0.0.1",
     author="Aditi Verma",
     author_email="aditiverma1605@gmail.com",
